dab8b37de37f887e56fda0b42a8c03c18ab12a17
LiliaBilous <liliyabilous.email@gmail.com>
2025-08-19 16:32:22 +0300

    feat: update hero section with new image and loading state handling

--- frontend/src/layouts/AppLayout.vue
+++ frontend/src/layouts/AppLayout.vue
@@ -2,21 +2,31 @@
 import AppFooter from '@/shared/components/ui/AppFooter.vue'
 import ModalHost from '@/shared/components/modal/ModalHost.vue'
 import ActiveTrack from '@/shared/components/ActiveTrack.vue'
-
+import { ref, onMounted } from 'vue';
 defineProps<{
     title: string
 }>()
+const isLoaded = ref(false);
+const onImageLoad = () => {
+    isLoaded.value = true;
+};
+onMounted(() => {
+    const imgElement = new Image();
+
+    if (imgElement.complete) {
+        onImageLoad();
+    }
+});
 </script>
 
 <template>
     <div class="layout">
         <section class="hero">
-            <div class="hero__background">
-                <picture>
-                    <source srcset="/image-desktop.avif 1400w, /image-mobile.avif 720w" type="image/avif">
-                    <source srcset="/image-desktop.webp 1400w, /image-mobile.webp 720w" type="image/webp">
-                    <img src="/image-desktop.avif" alt="Colorful audio waveform hero background">
-                </picture>
+            <div class="hero__background" :class="{ 'loaded': isLoaded }">
+                <img src="/image-25.avif" alt="Placeholder" class="placeholder" />
+
+                <img src="/image-desktop.avif" loading="lazy" alt="Colorful audio waveform hero background"
+                    @load="onImageLoad" />
             </div>
             <div class="hero__content">
                 <ActiveTrack />
